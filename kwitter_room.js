8f8f751b64cbc8914218df27a3c49eab72496783
saiprateek100 <91792757+saiprateek100@users.noreply.github.com>
2022-07-31 20:44:42 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,18 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+      apiKey: "AIzaSyAJujkV9dc8mrcmlXTkA0a4bNv-3pqS9CU",
+      authDomain: "kwitterprasad.firebaseapp.com",
+      databaseURL: "https://kwitterprasad-default-rtdb.firebaseio.com",
+      projectId: "kwitterprasad",
+      storageBucket: "kwitterprasad.appspot.com",
+      messagingSenderId: "762064138131",
+      appId: "1:762064138131:web:4af6356b8a6ecede6e8266"
+    };
+    
+    // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+
